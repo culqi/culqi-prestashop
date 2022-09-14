809e53fa8a2da957ec3dcd360f8bae20aae6f6a0
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-09-13 20:04:13 -0500

    url

--- culqi.php
+++ culqi.php
@@ -7,21 +7,21 @@ if (!defined('_PS_VERSION_'))
 
 define('CULQI_SDK_VERSION', '1.3.0');
 
-define('URLAPI_INTEG', 'https://integ-panel.culqi.com');
-define('URLAPI_PROD', 'https://panel.culqi.com');
+define('URLAPI_INTEG', 'https://qa-test-panel.culqi.xyz');
+define('URLAPI_PROD', 'https://qa-panel.culqi.xyz');
 
-define('URLAPI_INTEG_3DS', 'https://3ds.culqi.com');
-define('URLAPI_PROD_3DS', 'https://3ds.culqi.com');
+define('URLAPI_INTEG_3DS', 'https://3ds-qa.culqi.xyz/');
+define('URLAPI_PROD_3DS', 'https://3ds-qa.culqi.xyz');
 
-define('URLAPI_ORDERCHARGES_INTEG', 'https://api.culqi.com/v2');
-define('URLAPI_CHECKOUT_INTEG', 'https://checkout.culqi.com/js/v4');
+define('URLAPI_ORDERCHARGES_INTEG', 'https://qa-api.culqi.xyz/v2');
+define('URLAPI_CHECKOUT_INTEG', 'https://qa-checkout.culqi.xyz/js/v4');
 define('URLAPI_LOGIN_INTEG', URLAPI_INTEG.'/user/login');
 define('URLAPI_MERCHANT_INTEG', URLAPI_INTEG.'/secure/merchant/');
 define('URLAPI_MERCHANTSINGLE_INTEG', URLAPI_INTEG.'/secure/keys/?merchant=');
 define('URLAPI_WEBHOOK_INTEG', URLAPI_INTEG.'/secure/events');
 
-define('URLAPI_ORDERCHARGES_PROD', 'https://api.culqi.com/v2');
-define('URLAPI_CHECKOUT_PROD', 'https://checkout.culqi.com/js/v4');
+define('URLAPI_ORDERCHARGES_PROD', 'https://qa-api.culqi.xyz/v2');
+define('URLAPI_CHECKOUT_PROD', 'https://qa-checkout.culqi.xyz/js/v4');
 define('URLAPI_LOGIN_PROD', URLAPI_PROD.'/user/login');
 define('URLAPI_MERCHANT_PROD', URLAPI_PROD.'/secure/merchant/');
 define('URLAPI_MERCHANTSINGLE_PROD', URLAPI_PROD.'/secure/keys/?merchant=');
