5549aa6bfb935ba5202b1d8493451ff331535a38
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-09-25 02:46:24 -0500

    upda_order_id

--- controllers/front/postpayment.php
+++ controllers/front/postpayment.php
@@ -1,5 +1,8 @@
 <?php
 
+include_once dirname(__FILE__, 3) . '/libraries/culqi-php/lib/culqi.php';
+include_once dirname(__FILE__, 3) . '/culqi.php';
+
 class CulqiPostpaymentModuleFrontController extends ModuleFrontController
 {	
 
@@ -25,6 +28,14 @@ class CulqiPostpaymentModuleFrontController extends ModuleFrontController
         //$order_payment->card_expiration = Tools::getValue("transaction_id");
         //$order_payment->card_holder = Tools::getValue("transaction_id");
         $order_payment->update();
+        //
+        $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
+        $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
+        $args_charge = array(
+            'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
+         );
+
+         $culqi_charge = $culqi->Charges->update( $args_charge );
 
         Tools::redirect('index.php?controller=order-confirmation&id_cart=' . (int)$cart->id . '&id_module=' . (int)$this->module->id . '&id_order=' . $this->module->currentOrder . '&key=' . $customer->secure_key);
 
