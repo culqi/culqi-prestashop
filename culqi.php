ed7cd2a60ce2fe1ed1b93766a6ee16b5628850d5
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-10-10 19:34:27 -0500

    url

--- culqi.php
+++ culqi.php
@@ -8,14 +8,17 @@ define('CULQI_SDK_VERSION', '1.3.0');
 
 define('URLAPI_INTEG', 'https://integ-panel.culqi.com');
 define('URLAPI_PROD', 'https://panel.culqi.com');
+
 define('URLAPI_INTEG_3DS', 'https://3ds.culqi.com');
 define('URLAPI_PROD_3DS', 'https://3ds.culqi.com');
+
 define('URLAPI_ORDERCHARGES_INTEG', 'https://api.culqi.com/v2');
 define('URLAPI_CHECKOUT_INTEG', 'https://checkout.culqi.com/js/v4');
 define('URLAPI_LOGIN_INTEG', URLAPI_INTEG.'/user/login');
 define('URLAPI_MERCHANT_INTEG', URLAPI_INTEG.'/secure/merchant/');
 define('URLAPI_MERCHANTSINGLE_INTEG', URLAPI_INTEG.'/secure/keys/?merchant=');
 define('URLAPI_WEBHOOK_INTEG', URLAPI_INTEG.'/secure/events');
+
 define('URLAPI_ORDERCHARGES_PROD', 'https://api.culqi.com/v2');
 define('URLAPI_CHECKOUT_PROD', 'https://checkout.culqi.com/js/v4');
 define('URLAPI_LOGIN_PROD', URLAPI_PROD.'/user/login');
