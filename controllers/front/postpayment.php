adc9a4a0cea62568e99b95d152abb0a43c641dfe
JoseHCalderon <105760917+JoseHCalderon@users.noreply.github.com>
2022-11-07 22:46:53 -0500

    co

--- controllers/front/postpayment.php
+++ controllers/front/postpayment.php
@@ -15,40 +15,43 @@ class CulqiPostpaymentModuleFrontController extends ModuleFrontController
     {
         parent::initContent();
         $this->ajax = false;
+        $chargeid = Tools::getValue("chargeid");
+        if($chargeid!=null and !empty($chargeid) and !is_null($chargeid) and $chargeid != ''){
 
-        $cart = $this->context->cart;
-        $customer = new Customer($cart->id_customer);
-        $this->module->validateOrder((int)$cart->id, Configuration::get('CULQI_STATE_OK'), (float)$cart->getordertotal(true), 'Culqi', null, array(), (int)$cart->id_currency, false, $customer->secure_key);
+            $cart = $this->context->cart;
+            $customer = new Customer($cart->id_customer);
+            $this->module->validateOrder((int)$cart->id, Configuration::get('CULQI_STATE_OK'), (float)$cart->getordertotal(true), 'Culqi', null, array(), (int)$cart->id_currency, false, $customer->secure_key);
 
-        $id_order = Order::getOrderByCartId($this->context->cart->id);
+            $id_order = Order::getOrderByCartId($this->context->cart->id);
 
-        $order = new Order($id_order);
-        $order_payment_collection = $order->getOrderPaymentCollection();
+            $order = new Order($id_order);
+            $order_payment_collection = $order->getOrderPaymentCollection();
 
-        $order_payment = $order_payment_collection[0];
+            $order_payment = $order_payment_collection[0];
 
-        $order_payment->card_number = Tools::getValue("card_number").'-'.Tools::getValue("chargeid");
-        $order_payment->card_brand = Tools::getValue("card_brand");
-        $order_payment->transaction_id = Tools::getValue("chargeid");
-        $order_payment->update();
+            $order_payment->card_number = Tools::getValue("card_number").'-'.$chargeid;
+            $order_payment->card_brand = Tools::getValue("card_brand");
+            $order_payment->transaction_id = $chargeid;
+            $order_payment->update();
 
-        $culqiPretashop =  new Culqi();
-        $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
-        $enviroment_cart = $infoCheckout['enviroment_backend'];
-        $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
-        $args_charge = array(
-            'enviroment' => $enviroment_cart,
-            'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
-         );
+            $culqiPretashop =  new Culqi();
+            $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
+            $enviroment_cart = $infoCheckout['enviroment_backend'];
+            $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
+            $args_charge = array(
+                'enviroment' => $enviroment_cart,
+                'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
+            );
 
-        try{
-            $culqi_charge = $culqi->Charges->update( Tools::getValue("chargeid"), $args_charge );
-        }catch (Exception $e){
-            echo '<script type="text/javascript">console.log("Error en el update de cargo!"); </script>';
+            try{
+                $culqi_charge = $culqi->Charges->update( $chargeid, $args_charge );
+            }catch (Exception $e){
+                echo '<script type="text/javascript">console.log("Error en el update de cargo!"); </script>';
+            }
+
+            Tools::redirect('index.php?controller=order-confirmation&id_cart=' . (int)$cart->id . '&id_module=' . (int)$this->module->id . '&id_order=' . $this->module->currentOrder . '&key=' . $customer->secure_key);
+
         }
-
-        Tools::redirect('index.php?controller=order-confirmation&id_cart=' . (int)$cart->id . '&id_module=' . (int)$this->module->id . '&id_order=' . $this->module->currentOrder . '&key=' . $customer->secure_key);
-
     }
 
 }
--- controllers/front/registersale.php
+++ controllers/front/registersale.php
@@ -16,35 +16,39 @@ class CulqiRegisterSaleModuleFrontController extends ModuleFrontController
     {
         parent::initContent();
         $this->ajax = false;
+        $order_id = Tools::getValue("order_id");
+        if($order_id!=null and !empty($order_id) and !is_null($order_id) and $order_id != '')
+        {
 
-        $cart = $this->context->cart;
-        $customer = new Customer($cart->id_customer);
-        $this->module->validateOrder((int)$cart->id, Configuration::get('CULQI_STATE_PENDING'), (float)$cart->getordertotal(true), 'Culqi', null, array(), (int)$cart->id_currency, false, $customer->secure_key);
+            $cart = $this->context->cart;
+            $customer = new Customer($cart->id_customer);
+            $this->module->validateOrder((int)$cart->id, Configuration::get('CULQI_STATE_PENDING'), (float)$cart->getordertotal(true), 'Culqi', null, array(), (int)$cart->id_currency, false, $customer->secure_key);
 
-        $id_order = Order::getOrderByCartId($this->context->cart->id);
-        $order = new Order($id_order);
-        $order_payment_collection = $order->getOrderPaymentCollection();
+            $id_order = Order::getOrderByCartId($this->context->cart->id);
+            $order = new Order($id_order);
+            $order_payment_collection = $order->getOrderPaymentCollection();
 
-        $order_payment = $order_payment_collection[0];
-        $order_payment->transaction_id = Tools::getValue("order_id");
-        $order_payment->update();
-        $culqiPretashop =  new Culqi();
-        $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
-        $enviroment_cart = $infoCheckout['enviroment_backend'];
-        $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
-        $args_order = array(
-            'enviroment' => $enviroment_cart,
-            'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
-         );
+            $order_payment = $order_payment_collection[0];
+            $order_payment->transaction_id = $order_id;
+            $order_payment->update();
+            $culqiPretashop =  new Culqi();
+            $infoCheckout = $culqiPretashop->getCulqiInfoCheckout();
+            $enviroment_cart = $infoCheckout['enviroment_backend'];
+            $culqi = new Culqi\Culqi(array('api_key' => $infoCheckout['llave_secreta'] ));
+            $args_order = array(
+                'enviroment' => $enviroment_cart,
+                'metadata' => ["order_id" => $id_order, "sponsor" => "prestashop"],
+            );
 
-        try{
-            $culqi_order = $culqi->Orders->update( Tools::getValue("order_id"), $args_order );
-        }catch (Exception $e){
-            echo '<script type="text/javascript">console.log("Error en el update de cargo!"); </script>';
+            try{
+                $culqi_order = $culqi->Orders->update( $order_id, $args_order );
+            }catch (Exception $e){
+                echo '<script type="text/javascript">console.log("Error en el update de cargo!"); </script>';
+            }
+
+            die(Tools::jsonEncode($id_order));
+
         }
-
-        die(Tools::jsonEncode($id_order));
-
     }
 
 }
